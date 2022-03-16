3a7bc59e1cf01e14b184f6d6a126ce1316741012
robinrakibul <robinrakibulhassan@gmail.com>
2022-03-16 12:50:26 +0600

    added a counter component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 const products=[
   {name: 'laptop', price:153000},
@@ -11,15 +12,30 @@ const products=[
 function App() {
   return (
     <div className="App">
+      <Counter></Counter>
       {
         products.map(product => <Product name={product.name} price={product.price}></Product>)
       }
-      {/* <Product name="laptop" price="47000"></Product>
+      <Product name="laptop" price="47000"></Product>
       <Product name="phone" price="75000"></Product>
-      <Product name="watch" price="3500"></Product> */}
+      <Product name="watch" price="3500"></Product>
     </div>
   );
 }
+
+function Counter(){
+  const [count,setCount] = useState(33);
+  const increaseCount = () =>{ setCount(count+1); }
+  const decreaseCount = () =>{setCount(count-1);}
+  return (
+    <div>
+      <h1>Count: {count}</h1>
+      <button onClick={increaseCount}>Increase</button>
+      <button onClick={decreaseCount}>Decrease</button>
+    </div>
+  )
+}
+
 
 function Product(props){
   return (
